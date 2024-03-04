0b96a3f5062e597064436228686c3a4f9cffd5a4
Serhii Hudzenko <12gserij12@gmail.com>
2024-03-04 17:50:23 +0200

    fin

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -10,4 +10,9 @@ export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email({ minDomainSegments: 2 }),
   phone: Joi.string(),
-});
+})
+
+  .min(1)
+  .messages({
+    "object.min": "Body must have at least one field",
+  });
